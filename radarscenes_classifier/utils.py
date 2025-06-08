be7ef4219d82515d514ac06b215a9be0ad57277a
Jari Blumrich <jariblumrich98@gmail.com>
2025-06-08 16:46:16 +0200

    flake8-Verstöße behoben

--- radarscenes_classifier/utils.py
+++ radarscenes_classifier/utils.py
@@ -1,3 +1,7 @@
 # Hilfsfunktionen (z.B. Logging, allgemeine Helfer)
-# Sammelt Hilfsfunktionen, die in mehreren Modulen benötigt werden (z.B. eine Funktion zum Speichern/Laden von Modellen, Logging-Konfiguration, etc.).
-# Dadurch vermeidet man Duplizierung und hält den Code DRY – “Don't Repeat Yourself” steht dafür, Wiederholungen im Code durch Abstraktionen zu ersetzen
+# Sammelt Hilfsfunktionen, die in mehreren Modulen benötigt werden 
+# (z.B. eine Funktion zum Speichern/Laden von Modellen, 
+# Logging-Konfiguration, etc.).
+# Dadurch vermeidet man Duplizierung und hält den Code 
+# DRY – “Don't Repeat Yourself” steht dafür, Wiederholungen 
+# im Code durch Abstraktionen zu ersetzen
--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -1,8 +1,13 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from radarscenes_classifier import data_loading
+
+sys.path.insert(
+    0, 
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..")
+        )
+)
 
 RAW_DATA_DIR = os.path.join("dataset", "radar_scenes", "data")
 OUTPUT_DIR = os.path.join("dataset", "radar_scenes_pickles")
